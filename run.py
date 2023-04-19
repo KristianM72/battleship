1c2e7d2b0669dae9bbce3a4e8a45395cb49acd4b
Kristian Mikkonen <domainpro72@gmail.com>
2023-04-19 09:21:37 +0000

    Add game functions

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
 
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+"""
+Game functions
+"""
+def board():
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
